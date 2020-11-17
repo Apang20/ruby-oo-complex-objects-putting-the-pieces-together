ae1f88fb136f501de5beab37291ba42c3ad66d72
github username <github email address>
2020-11-17 16:49:27 -0600

    commit tree 7b183c6812ffb8fa3bfdbeaee3309731f049fe91

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,23 @@
-# Make your shoe class here!
+class Shoe 
+    attr_accessor :color, :size, :material, :condition
+    attr_reader :brand 
+
+    def initialize(brand)
+        @brand = brand 
+    end 
+
+    # def condition
+    #     @condition = condition
+    #end 
+    
+    def cobble 
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+        # if shoe.condition == "old"
+        # shoe.condition = "new"
+        #end 
+        #:condition == "new"
+end 
+end 
+
+ 
